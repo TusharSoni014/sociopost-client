93368443dc22e13e73bf87c90fbb45ea5a0b33a8
Tushar Soni <tusharsoni014@gmail.com>
2023-01-30 15:17:02 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -9,8 +9,13 @@ import store from "../redux/store";
 import { setLoading, showToast } from "../redux/slices/appConfigSlice";
 import { TOAST_ERROR } from "../App";
 
+let baseURL = 'http://localhost:4000'
+if(process.env.NODE_ENV === 'production'){
+  baseURL=process.env.REACT_APP_SERVER_BASE_URL
+}
+
 export const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_BASE_URL,
+  baseURL,
   withCredentials: true,
 });
 
